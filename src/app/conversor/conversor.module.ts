7c85ac5bca5beae8a3780500d58f736045637f76
teoitinga <teo.itinga@gmail.com>
2020-02-26 23:03:20 -0300

    Aula 108 - Seção 06 do curso angular 9 - Criando componente modal

--- src/app/conversor/conversor.module.ts
+++ src/app/conversor/conversor.module.ts
@@ -1,17 +1,21 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { ConversorComponent } from './components';
 import { MoedaService, ConversorService } from './services';
+import { NumeroDirective } from './directives';
 
 @NgModule({
   declarations: [
-    ConversorComponent
+    ConversorComponent,
+    NumeroDirective
   ],
   imports: [
     CommonModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule
   ],
   providers:[
     MoedaService,
